b97634265edd9b3c9d65f65178a378a05b0db5e9
Andres Varon <anvaron@gmail.com>
2022-08-22 09:43:45 -0400

    Project update

--- front-end/src/Pages/NotFound.js
+++ front-end/src/Pages/NotFound.js
@@ -1,7 +1,35 @@
+import { useNavigate } from "react-router-dom";
 export default function NotFound() {
+  let navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/snacks`);
+  }
+
+
   return (
-    <div>
-      <h1>404 Error, Page not found</h1>
+    <div class="bg-white  ">
+        <div class="text-center w-full mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
+            <h2 class="text-5xl font-extrabold text-black dark:text-white sm:text-4xl">
+                <span class="block text-red-400 text-8xl mt-10 mb-10">
+                    404 ERROR
+                </span>
+                <span class="block text-indigo-500">
+                  Page requested not found 
+                </span>
+            </h2>
+            <div class="lg:mt-0 lg:flex-shrink-0">
+                <div class="mt-12 inline-flex rounded-md shadow">
+                    <button 
+                      onClick={() => handleClick()}
+                      type="button" 
+                      class="py-4 px-6 bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                    >
+                      Go to Snacks
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
--- front-end/src/Pages/Show.js
+++ front-end/src/Pages/Show.js
@@ -3,8 +3,8 @@ import SnackDetails from "../Components/SnackDetails";
 export default function Show() {
   return (
     <main className="sm:flex flex-wrap justify-center items-center text-center">
-      <div class="header flex items-end justify-between mb-12 p-12">
-        <div class="title">
+      <div class="header w-full items-end justify-between p-12">
+        <div class=" title">
           <p class="text-4xl font-bold text-gray-800 mb-4">
           Snack details
           </p>

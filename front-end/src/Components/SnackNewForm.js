c7130bbe5f2ff45e9774f82f99db435c73291c2f
Andres Varon <anvaron@gmail.com>
2022-08-24 04:52:15 -0400

    Project update

--- front-end/src/Components/SnackNewForm.js
+++ front-end/src/Components/SnackNewForm.js
@@ -47,7 +47,7 @@ export default function SnackNewForm() {
   return (
     <div className="p-0 md:p-10 mt-32 md:mt-0 bg-gray-100 items-center ">
       <div className="flex items-center justify-center h-screen">
-        <div className="block p-10 bg-white w-full md:w-6/12 rounded-lg shadow-lg lg:p-12 lg:col-span-3">
+        <div className="block p-10 bg-white w-full md:w-6/12 mb-24 md:md-0 rounded-lg shadow-lg lg:p-12 lg:col-span-3">
           <form >
           <div className="mt-6 mb-6">
             <label htmlFor="name">Name:</label>

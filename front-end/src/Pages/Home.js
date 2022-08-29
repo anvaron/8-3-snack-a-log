157eeb90491197bad0461266839aaf013b5eadf1
Andres Varon <anvaron@gmail.com>
2022-08-29 12:15:13 -0400

    Project update

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -8,7 +8,7 @@ export default function Home() {
   }
 
   return (
-    <div className="bg-white  ">
+    <div className="h-full bg-white">
         <div className="text-center w-full mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
             <h2 className="text-5xl font-extrabold text-black dark:text-white sm:text-4xl">
                 <span className="block text-gray-500 text-6xl mt-10 mb-6">

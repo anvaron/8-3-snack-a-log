5c40422b462393876c4e2d2f7351d28bcf5e9a9a
Andres Varon <anvaron@gmail.com>
2022-08-29 13:39:46 -0400

    Project update

--- front-end/src/Components/SnackDetails.js
+++ front-end/src/Components/SnackDetails.js
@@ -51,23 +51,23 @@ export default function SnackDetails() {
               {snack.name}
             </h2>
             <h4 className="text-xl leading-8 font-extrabold text-gray-900 tracking-tight sm:leading-9">
-            <div className="flex-shrink-0 ">
-            <div className="container "
-            >
-            {snack.is_healthy 
-              ? 
-              <p className="w-48 object-center items-center px-4 py-2  text-base rounded-full text-green-600  bg-green-200">
-                Healthy Snack
-              </p> 
-              : 
-              <p className="w-48 object-center items-center px-4 py-2  text-base rounded-full text-red-600 bg-red-200 hover:bg-red-300">
-                Unhealthy Snack
-              </p>
-            }
-            </div>
-            </div>
+              <div className="flex-shrink-0 ">
+                <div className="container "
+                >
+                {snack.is_healthy 
+                  ? 
+                  <p className="w-48 object-center items-center px-4 py-2  text-base rounded-full text-green-600  bg-green-200">
+                    Healthy Snack
+                  </p> 
+                  : 
+                  <p className="w-48 object-center items-center px-4 py-2  text-base rounded-full text-red-600 bg-red-200 hover:bg-red-300">
+                    Unhealthy Snack
+                  </p>
+                }
+                </div>
+              </div>
             </h4>
-            <ul className="mt-10">
+            {/* <ul className="mt-10">
               <li>
                 <div className="flex">
                     <div className="flex-shrink-0">
@@ -116,7 +116,66 @@ export default function SnackDetails() {
                       </div>
                   </div>
               </li>
-            </ul>
+            </ul> */}
+
+            <div class="container flex flex-col mx-auto w-full items-center justify-center bg-white dark:bg-gray-800 rounded-lg shadow">
+              <ul class="flex flex-col divide divide-y">
+                <li class="flex flex-row">
+                  <div class="select-none cursor-pointer flex flex-1 items-center p-4">
+                    <div class="flex flex-col w-10 h-10 justify-center items-center mr-4">
+                      <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m2 4v16.002c0 .385.22.735.567.902.346.166.758.119 1.058-.121l4.725-3.781h12.65c.552 0 1-.448 1-1v-12.002c0-.552-.448-1-1-1h-18c-.552 0-1 .448-1 1zm18.5 11.502h-12.677l-4.323 3.46v-14.462h17zm-8.502-6.5c.414 0 .75.336.75.75v3.5c0 .414-.336.75-.75.75s-.75-.336-.75-.75v-3.5c0-.414.336-.75.75-.75zm.002-3c.552 0 1 .448 1 1s-.448 1-1 1-1-.448-1-1 .448-1 1-1z" fill-rule="nonzero"/></svg>
+                    </div>
+                    <div class="flex-1 pl-1 mr-16">
+                      <div class="font-medium dark:text-white">
+                        Fiber:
+                      </div>
+                      <div class="text-gray-600 dark:text-gray-200 text-sm">
+                          
+                      </div>
+                    </div>
+                    <div class="text-gray-600 dark:text-gray-200 text-xs">
+                      <span className="text-2xl font-bold text-indigo-500">{snack.fiber}g</span>
+                    </div>
+                  </div>
+                </li>
+                <li class="flex flex-row">
+                  <div class="select-none cursor-pointer flex flex-1 items-center p-4">
+                    <div class="flex flex-col w-10 h-10 justify-center items-center mr-4">
+                      <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m2 4v16.002c0 .385.22.735.567.902.346.166.758.119 1.058-.121l4.725-3.781h12.65c.552 0 1-.448 1-1v-12.002c0-.552-.448-1-1-1h-18c-.552 0-1 .448-1 1zm18.5 11.502h-12.677l-4.323 3.46v-14.462h17zm-8.502-6.5c.414 0 .75.336.75.75v3.5c0 .414-.336.75-.75.75s-.75-.336-.75-.75v-3.5c0-.414.336-.75.75-.75zm.002-3c.552 0 1 .448 1 1s-.448 1-1 1-1-.448-1-1 .448-1 1-1z" fill-rule="nonzero"/></svg>
+                    </div>
+                    <div class="flex-1 pl-1 mr-16">
+                      <div class="font-medium dark:text-white">
+                        Protein:
+                      </div>
+                      <div class="text-gray-600 dark:text-gray-200 text-sm">
+                      
+                      </div>
+                    </div>
+                    <div class="text-gray-600 dark:text-gray-200 text-xs">
+                      <span className="text-2xl font-bold text-indigo-500">{snack.protein}g</span>
+                    </div>
+                  </div>
+                </li>
+                <li class="flex flex-row">
+                  <div class="select-none cursor-pointer flex flex-1 items-center p-4">
+                    <div class="flex flex-col w-10 h-10 justify-center items-center mr-4">
+                      <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m2 4v16.002c0 .385.22.735.567.902.346.166.758.119 1.058-.121l4.725-3.781h12.65c.552 0 1-.448 1-1v-12.002c0-.552-.448-1-1-1h-18c-.552 0-1 .448-1 1zm18.5 11.502h-12.677l-4.323 3.46v-14.462h17zm-8.502-6.5c.414 0 .75.336.75.75v3.5c0 .414-.336.75-.75.75s-.75-.336-.75-.75v-3.5c0-.414.336-.75.75-.75zm.002-3c.552 0 1 .448 1 1s-.448 1-1 1-1-.448-1-1 .448-1 1-1z" fill-rule="nonzero"/></svg>
+                    </div>
+                    <div class="flex-1 pl-1 mr-16">
+                      <div class="font-medium dark:text-white">
+                        Added Sugar:
+                      </div>
+                      <div class="text-gray-600 dark:text-gray-200 text-sm">
+                      
+                      </div>
+                    </div>
+                    <div class="text-gray-600 dark:text-gray-200 text-xs">
+                      <span className="text-2xl font-bold text-indigo-500">{snack.added_sugar}g</span>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
             <div className="flex items-center mt-10">
               <button
                 onClick={() => handleClick()}
@@ -130,12 +189,12 @@ export default function SnackDetails() {
                 type="button" 
                 className="w-full border text-base font-bold bg-red-800 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 px-4 py-2"
               >
-                Delete
+                Delete Snack
               </button>
               <button
                 onClick={() => handleEdit()}
                 type="button" 
-                className="w-full border-t border-b border-r text-base font-bold rounded-r-md bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200  px-4 py-2"
+                className="w-full border-t border-b border-r text-base font-bold rounded-r-md bg-gray-800 hover:bg-gray-700 focus:ring-gray-500 focus:ring-offset-gray-200 text-white w-full transition ease-in duration-200  px-4 py-2"
               >
                 Edit Snack
               </button>

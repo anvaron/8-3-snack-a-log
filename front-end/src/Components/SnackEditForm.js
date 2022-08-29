704861ff3bcf4b00487f871865b63e8e67d4de76
Andres Varon <anvaron@gmail.com>
2022-08-29 14:19:55 -0400

    Project update

--- front-end/src/Components/SnackEditForm.js
+++ front-end/src/Components/SnackEditForm.js
@@ -179,13 +179,13 @@ export default function SnackEditForm() {
               "
             />
             </div>
-            <div className="flex items-center mt-10">
+            <div className="flex items-center mt-10 gap-2">
                   <button
                     onClick={() => handleClick()}
                     type="button" 
                     className="w-full border-l border-t border-b text-base font-medium rounded-l-md bg-gray-600 hover:bg-gray-700 focus:ring-gray-500 focus:ring-offset-gray-200 text-white w-full transition ease-in duration-200 px-4 py-4"
                   >
-                    Back
+                    <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 mx-auto object-center" fill="#FFFFFF" viewBox="0 0 24 24"><path d="M13.427 3.021h-7.427v-3.021l-6 5.39 6 5.61v-3h7.427c3.071 0 5.561 2.356 5.561 5.427 0 3.071-2.489 5.573-5.561 5.573h-7.427v5h7.427c5.84 0 10.573-4.734 10.573-10.573s-4.733-10.406-10.573-10.406z"/></svg>
                   </button>
                   
                   <button
@@ -193,7 +193,7 @@ export default function SnackEditForm() {
                     type="button" 
                     className="w-full border-t border-b border-r text-base font-medium rounded-r-md bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200  px-4 py-4"
                   >
-                    Submit
+                    <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 mx-auto object-center" fill="#FFFFFF" viewBox="0 0 24 24"><path d="M24 0l-6 22-8.129-7.239 7.802-8.234-10.458 7.227-7.215-1.754 24-12zm-15 16.668v7.332l3.258-4.431-3.258-2.901z"/></svg>
                   </button>
                 </div>
           </form>

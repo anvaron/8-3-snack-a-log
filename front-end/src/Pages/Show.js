c409603738259cce17c075f7b9c98c9a1770066b
Andres Varon <anvaron@gmail.com>
2022-08-29 12:13:20 -0400

    Project update

--- front-end/src/Pages/Show.js
+++ front-end/src/Pages/Show.js
@@ -2,7 +2,7 @@ import SnackDetails from "../Components/SnackDetails";
 
 export default function Show() {
   return (
-    <main className="sm:flex flex-wrap justify-center items-center text-center">
+    <main className="h-full sm:flex flex-wrap justify-center items-center text-center">
       <div className="header w-full items-end justify-between p-12">
         <div className=" title">
           <p className="text-4xl font-bold text-gray-800 mb-4">

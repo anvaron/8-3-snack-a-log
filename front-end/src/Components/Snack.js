4a26690fa15f61a9680bb3e834bbdf0a17723cab
Andres Varon <anvaron@gmail.com>
2022-08-29 12:09:45 -0400

    Project update

--- front-end/src/Components/Snack.js
+++ front-end/src/Components/Snack.js
@@ -41,7 +41,7 @@ export default function Snack({ snack }) {
         <button 
           onClick={() => handleClick()}
           type="button" 
-          className="py-2 px-4 bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+          className="py-2 px-4 bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-md ">
           Snack details 
         </button>
       </div>
